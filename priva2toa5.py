0a78b150a6e0f1225117b03059c960cb2a4a6e3b
Gerard Devine <g.devine@westernsydney.edu.au>
2018-01-09 16:16:51 +1100

    Adds 'TIMESTAMP' to header

--- priva2toa5.py
+++ priva2toa5.py
@@ -21,6 +21,14 @@ import credentials
 import logging
 
 
+# ----------------
+# Program settings
+# ----------------
+#  Set directory path for downloaded priva files (either relative or full pathname)
+priva_directory = os.path.join('.', 'priva_originals')
+#  Set directory path for converted TOA5 files (also where upload to HIEv will occur from)
+hiev_directory = os.path.join('.', 'converted')
+
 def mail_grab():
     """ Downloads file attachments from given gmail address
 
@@ -62,9 +70,8 @@ def mail_grab():
                 # yesterday = (datetime.date.today() - datetime.timedelta(1)).strftime('%Y%m%d')
 
                 if bool(file_name) and yesterday in file_name:
-                    file_path = os.path.join(detach_dir, 'priva_originals', file_name)
+                    file_path = os.path.join(priva_directory, file_name)
                     if not os.path.isfile(file_path):
-                        print(file_name)
                         fp = open(file_path, 'wb')
                         fp.write(part.get_payload(decode=True))
                         fp.close()
@@ -86,13 +93,6 @@ logging.info('Run Date - {}'.format(datetime.date.today()))
 logging.info('-------------------------')
 
 
-# Make sure required directories are in place
-detach_dir = '.'
-if 'priva_originals' not in os.listdir(detach_dir):
-    os.mkdir('priva_originals')
-if 'converted' not in os.listdir(detach_dir):
-    os.mkdir('converted')
-
 # Program works off of yesterday's data, sop create a variable to hold that date
 yesterday = (datetime.date.today() - datetime.timedelta(1)).strftime('%Y%m%d')
 
@@ -101,7 +101,7 @@ logging.info('Checking gmail account for new priva files....')
 mail_grab()
 
 yesterday_match = '*%s.csv' % yesterday
-priva_files = fnmatch.filter(os.listdir('priva_originals'), yesterday_match)
+priva_files = fnmatch.filter(os.listdir(priva_directory), yesterday_match)
 logging.info('{} new priva files downloaded from gmail'.format(len(priva_files)))
 
 # Modify downloaded priva CSV file to be HIEv-friendly
@@ -110,21 +110,25 @@ for priva_file in priva_files:
     # Extract Table name from file name
     table_name = re.split('\[|\]', str(priva_file))
     toa5df = pd.DataFrame(["TOA5", "R3_T1", "CR3000", "6550", "CR3000.Std.22", "CPU:R3_T1_Flux_20160803.CR3", "50271",
-                           table_name[1].replace(" ", "")]).T
+                           table_name[1].replace(" ", "").replace("#", "")]).T
 
     # Open original file and modify to make it TOA5/'HIEv-friendly'
-    with open(os.path.join('priva_originals', priva_file), newline='') as f:
+    with open(os.path.join(priva_directory, priva_file), newline='') as f:
         # Read in the Priva CSV file into dataframe
         r = pd.read_csv(f, sep=';', header=None)
         # Check if the Priva data contains midnight date from the next day and delete it if so
         if '00:00:00' in r.iloc[-1][0]:
             r = r[:-1]
+        # 'TIMESTAMP' needs to be in the header for proper hiev upload
+        r.iloc[0][0] = "TIMESTAMP"
         # Concatenate together the toa5 dataframe with the priva dataframe
         toa5_final = pd.concat([toa5df, r])
         # Modify the format of the date column to 'YYYY-MM-DD HH:MM:SS'
         toa5_final[0][4:] = toa5_final[0][4:].map(lambda x: datetime.datetime.strptime(x,
                                                   '%d-%m-%Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S'))
 
-    with open(os.path.join('converted', 'GHF_R_'+table_name[1].replace(" ", "")+'.csv'), 'w', newline='') as p:
+    with open(os.path.join(hiev_directory, 'GHF_R_'+table_name[1].replace(" ", "")
+                           .replace("#", "")+'.csv'), 'w', newline='') as p:
         toa5_final.to_csv(p, index=False, header=False)
-        logging.info("TOA5 file '{}' successfully created".format('GHF_R_'+table_name[1].replace(" ", "")+'.csv'))
+        logging.info("TOA5 file '{}' successfully created".format('GHF_R_'+table_name[1]
+                                                                  .replace(" ", "").replace("#", "")+'.csv'))
